3b2da8a6b96530ab276c451b0b48f816a68d2979
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2024-01-04 21:14:53 +0330

    Update BS4_Tagging (Unit 1).py

--- BS4_Tagging (Unit 1).py
+++ BS4_Tagging (Unit 1).py
@@ -152,3 +152,36 @@ bs44 = BeautifulSoup(link2, "html.parser")
 
 
 print(bs44)
+
+
+
+
+
+# %%
+
+# Some important points on regular expressions:
+
+# https://www.regexpal.com/
+# %%
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+
+html = urlopen("http://www.pythonscraping.com/pages/page3.html")
+bs = BeautifulSoup(html, "html.parser")
+
+images = bs.find_all("img", {"src": re.compile("\.\.\/img\/gifts/img.*\.jpg")})
+
+for image in images:
+    print(image["src"])
+
+bs.find_all(lambda tag: len(tag.attrs) == 2)
+# %%
+
+
+bs.find_all(lambda tag: tag.get_text() == "Or maybe he's only resting?")
+
+bs.find_all("", text="Or maybe he's only resting?")
+
+# %%
