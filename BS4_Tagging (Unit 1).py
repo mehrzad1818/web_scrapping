2986b7215ea3e5ac597f8a5288b41380cba4cf22
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2024-01-13 23:54:12 +0330

    Update BS4_Tagging (Unit 1).py

--- BS4_Tagging (Unit 1).py
+++ BS4_Tagging (Unit 1).py
@@ -285,3 +285,19 @@ random.seed(datetime.datetime.now()) def getLinks(articleUrl):
 html = urlopen('http://en.wikipedia.org{}'.format(articleUrl))
 bs = BeautifulSoup(html, 'html.parser')
 return bs.find('div', {'id':'bodyContent'}).find_all('a', href=re.compile('^(/wiki/)((?!:).)*$')) 
+
+
+
+
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+pages = set()
+def getLinks(pageUrl):
+global pages
+html = urlopen('http://en.wikipedia.org{}'.format(pageUrl))
+bs = BeautifulSoup(html, 'html.parser') for link in bs.find_all('a', href=re.compile('^(/wiki/)')): if 'href' in link.attrs: if link.attrs['href'] not in pages:
+#We have encountered a new page
+newPage = link.attrs['href'] print(newPage) pages.add(newPage) getLinks(newPage)
+getLinks('')
