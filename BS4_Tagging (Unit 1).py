6c5510de20e4401db41c0e3aa783999137d0ba95
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2024-01-13 23:53:41 +0330

    Update BS4_Tagging (Unit 1).py

--- BS4_Tagging (Unit 1).py
+++ BS4_Tagging (Unit 1).py
@@ -264,3 +264,13 @@ print(bs4_object.html.h1)
 # The same code as above, but with html5lib
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+
+
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+html = urlopen('http://en.wikipedia.org/wiki/Kevin_Bacon')
+bs = BeautifulSoup(html, 'html.parser')
+for link in bs.find('div', {'id':'bodyContent'}).find_all( 'a', href=re.compile('^(/wiki/)((?!:).)*$')):
+if 'href' in link.attrs: print(link.attrs['href'])
