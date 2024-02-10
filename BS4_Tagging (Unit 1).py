bee59100a805e687b326b89108c8b1bc992d8fea
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2024-02-10 23:57:08 +0330

    Update BS4_Tagging (Unit 1).py

--- BS4_Tagging (Unit 1).py
+++ BS4_Tagging (Unit 1).py
@@ -609,34 +609,5 @@ class QuotesSpider(scrapy.Spider):
 
 
 
-from pathlib import Path
-
-import scrapy
-
-
-class QuotesSpider(scrapy.Spider):
-    name = "quotes"
-    start_urls = [
-        "https://quotes.toscrape.com/page/1/",
-        "https://quotes.toscrape.com/page/2/",
-    ]
-
-    def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = f"quotes-{page}.html"
-        Path(filename).write_bytes(response.body)
-
-
-
-for quote in response.css("div.quote"):
-    text = quote.css("span.text::text").get()
-    author = quote.css("small.author::text").get()
-    tags = quote.css("div.tags a.tag::text").getall()
-    print(dict(text=text, author=author, tags=tags))
-
-
-Code UPdated Up to Here
-
-
 The conversation will be held with Mr. Sanaati
 This song is not
